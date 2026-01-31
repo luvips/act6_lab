07cf27a123c9f8dab5e396656a0c7c446e706129
luvia <luviaamagaali07@gmail.com>
2026-01-31 01:16:38 -0600

    Agregar permisos al usuario

--- db/roles.sql
+++ db/roles.sql
@@ -8,4 +8,13 @@
 CREATE ROLE report_user WITH LOGIN PASSWORD 'password123';
 GRANT CONNECT ON DATABASE postgres TO report_user;
 
+--Permisos
+GRANT SELECT ON view_ranking_products TO report_user;
+GRANT SELECT ON view_sales_by_category TO report_user;
+GRANT SELECT ON view_top_customers TO report_user;
+GRANT SELECT ON view_inventory_status TO report_user;
+GRANT SELECT ON view_user_activity TO report_user;
+GRANT SELECT ON view_order_summary TO report_user;
+GRANT SELECT ON view_daily_sales TO report_user;
+
 SELECT rolname FROM pg_roles WHERE rolname = 'report_user';
