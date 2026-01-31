33f7413993e4393795dc42789b416b45a44a6516
luvia <luviaamagaali07@gmail.com>
2026-01-30 22:30:53 -0600

    Realizar mi primera view "Ranking de Productos"

---
+++ db/reports_vw.sql
@@ -0,0 +1,79 @@
+-- ============================================================
+-- Luvia Magali Hidalgo García 243732
+-- Tarea 6: Lab Reportes: Next.js ReportsDashboard (PostgreSQL + Views + Docker Compose)
+-- Archivo: db/reports_vw.sql
+-- 5 B
+-- ============================================================
+
+
+-- REPORTE 1: Ranking de Productos 
+-- VISTA: view_ranking_products 
+-- Qué devuelve: Lista de productos del más vendido al menos vendido.
+-- Grain: Un producto por fila.
+-- Métrica (s): SUM(orden_detalles.cantidad), RANK()
+-- Por qué GROUP BY / HAVING: Suma unidades por producto y oculta los que no tienen ventas.
+
+-- QUERY
+CREATE OR REPLACE VIEW vista_ranking_productos AS
+SELECT 
+    p.nombre AS producto,
+    SUM(od.cantidad) AS unidades_vendidas,
+    RANK() OVER (ORDER BY SUM(od.cantidad) DESC) AS lugar_ranking
+FROM productos p
+JOIN orden_detalles od ON p.id = od.producto_id
+GROUP BY p.id, p.nombre
+HAVING SUM(od.cantidad) > 0;
+
+-- VERIFY
+
+
+
+-- REPORTE : 
+-- VISTA: 
+-- Qué devuelve: 
+-- Grain: 
+-- Métrica (s): 
+-- Por qué GROUP BY / HAVING / subconsulta:
+-- QUERY
+-- VERIFY
+
+
+-- REPORTE : 
+-- VISTA: 
+-- Qué devuelve: 
+-- Grain: 
+-- Métrica (s): 
+-- Por qué GROUP BY / HAVING / subconsulta:
+-- QUERY
+-- VERIFY
+
+
+-- REPORTE : 
+-- VISTA: 
+-- Qué devuelve: 
+-- Grain: 
+-- Métrica (s): 
+-- Por qué GROUP BY / HAVING / subconsulta:
+-- QUERY
+-- VERIFY
+
+
+-- REPORTE : 
+-- VISTA: 
+-- Qué devuelve: 
+-- Grain: 
+-- Métrica (s): 
+-- Por qué GROUP BY / HAVING / subconsulta:
+-- QUERY
+-- VERIFY
+
+
+-- REPORTE : 
+-- VISTA: 
+-- Qué devuelve: 
+-- Grain: 
+-- Métrica (s): 
+-- Por qué GROUP BY / HAVING / subconsulta:
+-- QUERY
+-- VERIFY
+
